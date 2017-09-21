57512032abb5e67b0d58a9dec383f48f9b7b99fb
Jon Ziefle <jon.ziefle@gmail.com>
2017-09-20 22:34:54 -0400

    add basic looping

--- scripts/exec.py
+++ scripts/exec.py
@@ -1,13 +1,28 @@
 import os
-import process_names
+import json
 
-inputFiles = ['../data/baby-names/yob2016.txt']
-outputFile = '../data/2016.json'
+def main():
+    inputDir = '../data/baby-names'
+    inputFiles = ['../data/baby-names/yob2016.txt','../data/baby-names/yob2015.txt']
+    outputFile = '../data/2016.json'
 
-'''
-for file in os.listdir(inputDir):
-    if file.endswith('.txt'):
-        print(file)
-'''
+    data = {}
+    count = 0
+    for file in os.listdir(inputDir):
+        if file.endswith('.txt'):
+            if (count % 10 == 0):
+                key = file.replace('yob', '').replace('.txt', '') + 's'
+                data[key] = []
 
-process_names.main(inputFiles, outputFile)
+            data[key].append(file)
+            count += 1
+
+    print(json.dumps(data, sort_keys=True, indent=2))
+
+    #print(list(chunks(os.listdir(inputDir), 5)))
+
+    #process_names.main(inputFiles, outputFile)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
